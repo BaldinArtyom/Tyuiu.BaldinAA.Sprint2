d99d6490118dbc1590e83cae53e27baf98bab1f4
Tyuiu.BaldinAA@outlook.com <artyombaldin94438@gmail.com>
2024-10-18 01:13:04 +0500

    Task6

---
+++ Tyuiu.BaldinAA.Sprint2.Task6.V3/Program.cs
@@ -0,0 +1,35 @@
+﻿using Tyuiu.BaldinAA.Sprint2.Task6.V3.Lib;
+namespace Tyuiu.BaldinAA.Sprint2.Task6.V3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #2 | Выполнил: Балдин А. А. | ИБКСб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Алгоритмы разветвляющейся структуры                               *");
+            Console.WriteLine("* Задание #6                                                              *");
+            Console.WriteLine("* Вариант #3                                                              *");
+            Console.WriteLine("* Выполнил: Балдин Артём Александрович | ИБКСб-24-1                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая использует сокращенную форму записи         *");
+            Console.WriteLine("* оператора switch и определяет день недели по номеру.                    *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Номер дня недели:");
+            int value = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine($"{ds.FindDayName(value)}"); ;
+        }
+    }
+}
