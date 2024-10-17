1e69535e4615b55fc8c845031c8c60afb18163dd
Tyuiu.BaldinAA@outlook.com <artyombaldin94438@gmail.com>
2024-10-17 23:30:34 +0500

    Task5

---
+++ Tyuiu.BaldinAA.Sprint2.Task5.V9/Program.cs
@@ -0,0 +1,38 @@
+﻿using Tyuiu.BaldinAA.Sprint2.Task5.V9.Lib;
+namespace Tyuiu.BaldinAA.Sprint2.Task5.V9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #2 | Выполнил: Балдин А. А. | ИБКСб-24-1";
+            Console.WriteLine("************************************************************************");
+            Console.WriteLine("* Спринт #2                                                            *");
+            Console.WriteLine("* Тема:                                                                *");
+            Console.WriteLine("* Задание #5                                                           *");
+            Console.WriteLine("* Вариант #9                                                           *");
+            Console.WriteLine("* Выполнил: Балдин Артём Александрович | ИБКСб-24-1                    *");
+            Console.WriteLine("************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                             *");
+            Console.WriteLine("*                                                                      *");
+            Console.WriteLine("************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                     *");
+            Console.WriteLine("************************************************************************");
+
+            Console.WriteLine("Введите переменную m:");
+            int m = int.Parse(Console.ReadLine()!);
+
+            Console.WriteLine("Введите переменную n:");
+            int n = int.Parse(Console.ReadLine()!);
+
+            Console.WriteLine("************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                           *");
+            Console.WriteLine("************************************************************************");
+
+            Console.WriteLine(ds.FindDateOfNextDay(m, n)); ;
+        }
+    }
+}
