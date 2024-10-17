b70be925bd6dba60d038e8589aed65f118bfde24
Tyuiu.BaldinAA@outlook.com <artyombaldin94438@gmail.com>
2024-10-17 21:22:05 +0500

    Task1

---
+++ Tyuiu.BaldinAA.Sprint2.Task1.V27/Program.cs
@@ -0,0 +1,48 @@
+﻿using Tyuiu.BaldinAA.Sprint2.Task1.V27.Lib;
+namespace Tyuiu.BaldinAA.Sprint2.Task1.V27
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            int a = 654;
+            int b = 325;
+            int c = 154;
+            int d = 333;
+
+            bool[] res = new bool[6];
+            res = ds.GetLogicOperations(a, b, c, d);
+
+            Console.Title = "Спринт #1 | Выполнил: Балдин А. А. | ИБКСб-24 -1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Базовый навыки работы в C#                                        *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #27                                                             *");
+            Console.WriteLine("* Выполнил: Балдин Артём Алекчандрович | ИБКСб-24-1                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу из операций сравнений и и логических операций,       *");
+            Console.WriteLine("* а также арифметических выражений, которая вернет логическую             *");
+            Console.WriteLine("* последовательность: (True, False, True, False, True, False)             *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("a = " + a);
+            Console.WriteLine("b = " + b);
+            Console.WriteLine("c = " + c);
+            Console.WriteLine("d = " + d);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine(res[i]);
+            }
+        }
+    }
+}
